54a6f6dfc7055ff53526ea7150d563c8e2c4feb4
Neha Aggarwal <officialnehaaggarwal@gmail.com>
2026-04-02 00:20:13 +0530

    First Commit

---
+++ lib/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:my_first_project/screens/home_screen.dart';
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'my_first_app',
+      home: HomeScreen(),
+    );
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: const Text('My First Project',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 24,color: Colors.white),)),
+        backgroundColor: Colors.black,
+      ),
+      body: const Center(
+        child: Text('This is my first application',style: TextStyle(fontSize: 22),),
+      ),
+    );
+  }
+}
